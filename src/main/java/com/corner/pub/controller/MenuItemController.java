45a32a93fd44b430a167b0432de15594bce711a0
sanfelix2004 <sanfelicefrancesco004@gmail.com>
2025-07-16 16:44:46 +0200

    f: added cors annotation on controller menu

--- src/main/java/com/corner/pub/controller/MenuItemController.java
+++ src/main/java/com/corner/pub/controller/MenuItemController.java
@@ -8,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(
+        origins = "https://corner-frontend.onrender.com",
+        allowCredentials = "true"
+)
 @RestController
 @RequestMapping("/api/menu")
 public class MenuItemController {
