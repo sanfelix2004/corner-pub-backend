0b2eed390dc93b0d9cbe6844af47ec6ea140557f
sanfelix2004 <sanfelicefrancesco004@gmail.com>
2025-07-16 15:11:11 +0200

    Update WebConfig.java

--- src/main/java/com/corner/pub/config/WebConfig.java
+++ src/main/java/com/corner/pub/config/WebConfig.java
@@ -1,4 +1,4 @@
-\package com.corner.pub.config;
+package com.corner.pub.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
