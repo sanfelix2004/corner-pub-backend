098cd7ad93ecca9863eefa5407368efac4f870fb
sanfelix2004 <sanfelicefrancesco004@gmail.com>
2025-07-16 15:22:45 +0200

    :fix: fixed cors

--- src/main/java/com/corner/pub/config/WebConfig.java
+++ src/main/java/com/corner/pub/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://corner-frontend.onrender.com")
+                .allowedOrigins("https://corner-frontend.onrender.com", "https://corner-frontend.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
