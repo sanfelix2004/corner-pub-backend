8335364eb819b4af65a244ddc00831370a4728aa
sanfelix2004 <sanfelicefrancesco004@gmail.com>
2025-07-18 11:41:57 +0200

    fix: added cors to in evidenza controller

--- src/main/java/com/corner/pub/controller/InEvidenzaController.java
+++ src/main/java/com/corner/pub/controller/InEvidenzaController.java
@@ -2,12 +2,16 @@ package com.corner.pub.controller;
 
 import com.corner.pub.dto.response.InEvidenzaResponse;
 import com.corner.pub.service.InEvidenzaService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(
+        origins = "https://corner-frontend.onrender.com",
+        allowCredentials = "true"
+)
 @RestController
 @RequestMapping("/api/in_evidenza")
 public class InEvidenzaController {
