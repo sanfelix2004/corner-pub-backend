843e81eb096fa0b0491dd5bebdcebab6f9a459e6
sanfelix2004 <sanfelicefrancesco004@gmail.com>
2025-08-26 15:41:41 +0200

    fix wow

--- src/main/java/com/corner/pub/service/ReservationService.java
+++ src/main/java/com/corner/pub/service/ReservationService.java
@@ -194,22 +194,13 @@ public class ReservationService {
     }
 
     private LocalDate parseDate(String dateString) {
-        String cleanedDate = dateString.split("T")[0]; // Se arriva yyyy-MM-ddTHH:mm:ss
-        cleanedDate = cleanedDate.split(" ")[0]; // Se arriva yyyy-MM-dd HH:mm:ss
-
         DateTimeFormatter[] formatters = new DateTimeFormatter[]{
-                DateTimeFormatter.ISO_LOCAL_DATE,              // yyyy-MM-dd
-                DateTimeFormatter.ofPattern("dd-MM-yyyy"),     // dd-MM-yyyy
-                DateTimeFormatter.ofPattern("MM/dd/yyyy"),     // MM/dd/yyyy
-                DateTimeFormatter.ofPattern("yyyy/MM/dd"),     // yyyy/MM/dd
-                DateTimeFormatter.ofPattern("dd/MM/yyyy"),     // dd/MM/yyyy
-                DateTimeFormatter.ISO_DATE,                    // yyyy-MM-dd+timezone
-                DateTimeFormatter.ISO_DATE_TIME                // yyyy-MM-ddTHH:mm:ss
+                DateTimeFormatter.ISO_LOCAL_DATE
         };
 
         for (DateTimeFormatter formatter : formatters) {
             try {
-                return LocalDate.parse(cleanedDate, formatter);
+                return LocalDate.parse(dateString, formatter);
             } catch (DateTimeParseException ignored) {
             }
         }
